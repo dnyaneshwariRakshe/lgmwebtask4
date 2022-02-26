0e29b9ef29c4704437ae67fb6e3520ff866d1267
dnyaneshwariRakshe <rakshednyaneshwari@gmail.com>
2022-02-26 22:19:21 +0530

    first commit

---
+++ calcul.js
@@ -0,0 +1,16 @@
+
+function enin(userin){
+    var uin = document.getElementsByTagName("input")[0].value;
+    var upd=uin+userin;
+    document.getElementsByTagName("input")[0].value=(upd);
+    }
+    
+    function clearb(){
+    document.getElementsByTagName("input")[0].value="";
+    }
+    
+    function solve(){
+    var calcScreen = document.getElementsByTagName("input")[0].value;
+    var upd = eval(calcScreen);
+    document.getElementsByTagName("input")[0].value=(upd);
+    }
